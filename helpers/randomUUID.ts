d227cd2de1c2efb51c1f1570e8d4e1d69239841d
gjongenelen <gino@jongenelen.net>
2023-03-27 14:05:55 +0200

    fix polyfill

--- helpers/randomUUID.ts
+++ helpers/randomUUID.ts
@@ -1,13 +1,6 @@
 
-let randomUUID = crypto.randomUUID
-
-// @ts-ignore
-if (typeof randomUUID === 'undefined')
-    randomUUID = function randomUUID() {
-        // @ts-ignore
-        return ( [1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        );
-    };
-
-
-export default randomUUID;
+export default function() {
+    // @ts-ignore
+    return ( [1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+};
