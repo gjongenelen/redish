a7e982f7e5f7fdc8bb99d8373be5a2c3fee1a071
gjongenelen <gino@jongenelen.net>
2023-08-05 13:52:49 +0200

    debug lock

--- src/helpers/lock.ts
+++ src/helpers/lock.ts
@@ -4,10 +4,13 @@ export class Lock {
     private acquired = false;
 
     public async acquire(): Promise<void> {
+        console.warn(`acquire() called`)
         if (!this.acquired) {
+            console.warn(`lock set to true`)
             this.acquired = true;
             return new Promise<void>(res => res());
         } else {
+            console.warn(`lock already true, queueing`)
             return new Promise<void>((resolve, _) => {
                 this.queue.push(resolve);
             });
@@ -22,9 +25,11 @@ export class Lock {
 
         const continuation = this.queue.shift();
         if (!continuation) {
+            console.warn(`lock set to false, exiting`)
             this.acquired = false;
             return;
         }
+        console.warn("continuing queue")
 
         continuation();
     }
