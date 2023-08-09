64f3edc10f901af9d8cd0162b5fb4b19b928131b
gjongenelen <gino@jongenelen.net>
2023-08-09 14:33:29 +0200

    bump to 1.0.9

--- src/helpers/lock.ts
+++ src/helpers/lock.ts
@@ -4,13 +4,10 @@ export class Lock {
     private acquired = false;
 
     public async acquire(): Promise<void> {
-        console.warn(`acquire() called`)
         if (!this.acquired) {
-            console.warn(`lock set to true`)
             this.acquired = true;
             return new Promise<void>(res => res());
         } else {
-            console.warn(`lock already true, queueing`)
             return new Promise<void>((resolve, _) => {
                 this.queue.push(resolve);
             });
@@ -25,11 +22,9 @@ export class Lock {
 
         const continuation = this.queue.shift();
         if (!continuation) {
-            console.warn(`lock set to false, exiting`)
             this.acquired = false;
             return;
         }
-        console.warn("continuing queue")
 
         continuation();
     }
